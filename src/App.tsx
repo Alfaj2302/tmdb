ec3236e4bb5480c32e646e13dc4a9d1b09b1e905
Your Alfaj2302 <alfaj.shaikh@tekditechnologies.com>
2025-04-14 14:26:18 +0530

    Login Screen Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import './App.css';
+
 function App() {
-  const {
-    t,
-    i18n: { changeLanguage, language },
-  } = useTranslation();
-  const [currentLanguage, setCurrentLanguage] = useState(language);
-  const handleChangeLanguage = () => {
-    const newLanguage = currentLanguage === 'en' ? 'pt' : 'en';
-    setCurrentLanguage(newLanguage);
-    changeLanguage(newLanguage);
-  };
   return (
-    <div className="App">
-      <h1>
-        Our Translated Header:
-        {t('headerTitle', { appName: 'App for Translations' })}
-      </h1>
-      <h3>Current Language: {currentLanguage}</h3>
-      <button type="button" onClick={handleChangeLanguage}>
-        Change Language
-      </button>
-    </div>
+    <Router basename="/tmdb_dummy">
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
